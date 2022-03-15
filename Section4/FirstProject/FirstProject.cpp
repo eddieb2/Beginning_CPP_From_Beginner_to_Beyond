765f0b7fb4eb1778cccac0aadced0e828baae24b
Edward Blanciak <blanciak.edward2@gmail.com>
2022-03-14 22:56:28 -0400

    Add project files.

---
+++ Section4/FirstProject/FirstProject.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+	int num;
+	std::cout << "Please enter a number: ";
+	std::cin >> num;
+	std::cout << "That is my favorite number too." << std::endl;
+
+	return 0;
+}
