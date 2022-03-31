af74b22a61317ea14c7a1200312eda2a3d319634
Edward Blanciak <blanciak.edward2@gmail.com>
2022-03-30 23:09:11 -0400

    Challenege assignment complete

--- Section 10/Challenge Assignment/Challenge Assignment.cpp
+++ Section 10/Challenge Assignment/Challenge Assignment.cpp
@@ -18,66 +18,93 @@ void printPyramid(vector<vector<char>> pyramid)
 }
 
 
-
 int main()
 {
-    // take a string from the user
-    string text{"ABC"};
-    //cout << "Enter a text: ";
-    //cin >> text;
+    // User input
+    string text{};
+    cout << "Enter a text: ";
+    cin >> text;
+    cout << endl;
     
-    vector <vector<char>> pyramid{ 
-        {' ',' ','A',' ',' '},
-        {' ','A','B','A',' '},
-        {'A','B','C','B','A'}
-    };
-
+    // Blank Vector
+    vector <vector<char>> pyramid{};
     size_t numberOfRows = text.length();
-    size_t numberOfColumns = numberOfRows + 2;
+    size_t numberOfColumns = (numberOfRows * 2)-1;
     int midpoint = numberOfColumns % numberOfRows;
 
-    // Blank grid creation 
-    //for (size_t i = 0; i < numberOfRows; i++)
-    //{
-    //    // Row creation - define row vector, pushback elements into row, pushback entire new row into pyramid
-    //    vector <char> row{};
-
-    //    for (size_t j = 0; j < numberOfColumns; j++)
-    //    {
-    //        row.push_back(' '); // substitute a letter for visual clarity
-    //    }
-
-    //    pyramid.push_back(row);
-    //}
-    
-    // midpoint in the first vector will always be the first letter in the text
-    pyramid[0][midpoint] = text[0];
-
-    // Insert elements into grid
-    for (size_t i = 0; i < pyramid.size(); i++) // looping from row to row
+    // Blank Grid creation 
+    for (size_t i = 0; i < numberOfRows; i++)
     {
-        
-        // looping from column to column within a particular row (iterate through each element in a row)
-        // need to start from the rows midpoint, go left , go right , insert elements
+        vector <char> row{};
 
-
-        // LOOP LEFT // l == 2
-        for (size_t l = midpoint-1; l > 0 ; l--)
+        for (size_t j = 0; j < numberOfColumns; j++)
         {
-            cout << pyramid[i][l] << " ";
+            row.push_back(' '); // substitute a letter for visual clarity
         }
 
-        cout << "MIDPOINT" << " ";
+        pyramid.push_back(row);
+    }
 
-        // LOOP RIGHT
-        for (size_t r = midpoint+1; r < numberOfColumns; r++)
+    // Insert Elements int midpoint positions
+    int count{ 0 }; 
+
+    for (size_t i = 0; i < pyramid.size(); i++)
+    {
+        // Adds elements to appropriate midpoints in each row.
+        pyramid[i][midpoint] = text[count];
+        count++;
+    }
+
+    // Left side of midpoint loop -- inserts appropriate elements left of the midpoint
+    for (size_t i = 0; i < pyramid.size(); i++)
+    {
+        int count{1};
+        int num_char_in_row{0};
+
+        // Starts one index to the left of midpoint 
+        for (size_t j = midpoint; j --> 0;)
         {
-            cout << pyramid[i][r] << " ";
+            // Stop condition: Stops the loop if we're in the first row or if there are no more characters to insert in the direction we're heading. The iterator "i" also represents how many characters should be inserted to the left and right of the midpoint. If num_char_in_row is greater than or equal to 'i', we've inserted enough characters.
+            if (i == 0 || (num_char_in_row >= i))
+            {
+                break;
+            }
+
+            // Locates the appropriate character from "text."
+            pyramid[i][j] = text[i - count];
+            
+            count++;
+            num_char_in_row++;
         }
     }
 
-    //printPyramid(pyramid);
+
+    // Right side of midpoint loop -- inserts appropriate elements right of the midpoint
+    for (size_t i = 0; i < pyramid.size(); i++)
+    {        
+        int count{ 1 };
+        int num_char_in_row{ 0 };
+
+        for (size_t j = (midpoint + 1); i < (numberOfColumns - 1); j++)
+        {
+            // Stop condition: Stops the loop if we're in the first row or if there are no more characters to insert in the direction we're heading. The iterator "i" also represents how many characters should be inserted to the left and right of the midpoint. If num_char_in_row is greater than or equal to 'i', we've inserted enough characters.
+            if (i == 0 || (num_char_in_row >= i))
+            {
+                break;
+            }
+
+            // Locates the appropriate character from "text."
+            pyramid[i][j] = text[i - count];
+            
+            count++; 
+            num_char_in_row++;
+
+        }
+    }
+
+    printPyramid(pyramid);
  
+    cout << endl;
     return 0;
 }
 
@@ -101,8 +128,8 @@ User Input - ABC
 
 4 rows x 7 columns
 User Input - 1234
-[   1   ] 4th element - 3rd index = midpoint ------ #columns % #rows == index of midpoint ----- 7%4 = 3
-[  121  ]
+[   1   ] 4th element - 3rd index = midpoint ------ #columns % #rows == index of midpoint -----
+[  121  ]  7%4 = 3
 [ 12321 ]
 [1234321]
 
